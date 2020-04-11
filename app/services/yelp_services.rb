169de34bb86bdb5df721eab8296e13177e82313d
Matt Holmes <holmesm8@gmail.com>
2020-04-11 11:59:34 -0600

    Google API is returning our rating request

--- app/services/yelp_services.rb
+++ app/services/yelp_services.rb
@@ -1,11 +1,22 @@
 class YelpService
 
-  def initialize(latitude, longitude)
-    api = Faraday.new("https://api.yelp.com/v3/businesses/search?latitude=#{latitude}&longitude=#{longitude}") do |conn|
+  def initialize(latitude, longitude, radius, price)
+    api = Faraday.new("https://api.yelp.com/v3/businesses/search?latitude=#{latitude}&longitude=#{longitude}&radius=#{radius}&price=#{price}") do |conn|
       conn.authorization :Bearer, ENV["YELP_API_KEY"]
     end
     response = api.get
-    json_response = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+    @json_response = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def random_restaurant
+    @json_response[:businesses].shuffle.pop
+  end
+
+  def google_api
+    phone = random_restaurant[:phone][1..-1]
+    api = Faraday.new("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%2B#{phone}&inputtype=phonenumber&fields=rating&key=#{ENV["GOOGLE_API_KEY"]}")
+    response = api.get
+    test = JSON.parse(response.body, symbolize_names: true)
+    test[:candidates][0][:rating]
   end
 end
--- spec/requests/yelp_request_spec.rb
+++ spec/requests/yelp_request_spec.rb
@@ -3,6 +3,8 @@ require './app/services/yelp_services'
 
 RSpec.describe 'Yelp API', type: :request do
   it "can create a service" do
-    service = YelpService.new(39.7392, -104.9903)
+    #latitude, longitude, radius, price parameters
+    service = YelpService.new(39.7392, -104.9903, 1000, 3)
+    google = service.google_api
   end
 end
