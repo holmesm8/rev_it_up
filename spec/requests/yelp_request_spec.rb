3315dad2b196c02b2d2f4370ffbc2dc278d0600b
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-04-16 11:36:21 -0600

    clean up test

--- spec/requests/yelp_request_spec.rb
+++ spec/requests/yelp_request_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe 'Rev it up api' do
     Server
   end
 
-  it "hits this pry" do
-    restaurant_params = {latitude: 39.7392, longitude: -104.9903, radius: 1000, price: "3"}
+  context 'GET /search' do
+    it "returns json response from google and yelp api calls" do
+      restaurant_params = {latitude: 39.7392, longitude: -104.9903, radius: 1000, price: "3"}
 
-    get '/search', restaurant_params
+      get '/search', restaurant_params
 
-    expect(last_response).to be_successful
-    response = JSON.parse(last_response.body)
+      expect(last_response).to be_successful
+    end
   end
 end
